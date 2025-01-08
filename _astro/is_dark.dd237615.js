25bd3da3a303d4cbabfe6c325f48b27d36fb965c
ouraihub <ouraihub@users.noreply.github.com>
2025-01-08 14:17:20 +0000

    Deploy: first commit Astro-template/astro-notion-blog@16940e5cfc8add918a194ff43e0023697fec11a5

---
+++ _astro/is_dark.dd237615.js
@@ -0,0 +1,1 @@
+import{a3 as c,a4 as a}from"./hoisted.9c37f0a1.js";const e=n=>{const{r,g:t,b:o}=c.parse(n),s=.2126*a.channel.toLinear(r)+.7152*a.channel.toLinear(t)+.0722*a.channel.toLinear(o);return a.lang.round(s)},i=e,l=n=>i(n)>=.5,u=l,h=n=>!u(n),L=h;export{L as i};
